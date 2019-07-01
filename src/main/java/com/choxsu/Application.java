7ee7034aefd9501672475a7b091b35381e3a44d2
choxsu <choxsu@163.com>
2019-07-01 22:53:14 +0800

    spring boot demo application update runAfterRunner application.yml

--- src/main/java/com/choxsu/Application.java
+++ src/main/java/com/choxsu/Application.java
@@ -1,8 +1,8 @@
 package com.choxsu;
 
 import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * @author choxsu
@@ -13,16 +13,16 @@ public class Application {
 
     public static void main(String[] args) {
         // Startup mode 1
-//        SpringApplication application = new SpringApplication(Application.class);
-//        // off banner mode
-//        application.setBannerMode(Banner.Mode.OFF);
-//        application.run(args);
+        SpringApplication application = new SpringApplication(Application.class);
+        // off banner mode
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
 
         // Startup mode 2
-        new SpringApplicationBuilder()
-                .sources(Application.class)
-                .bannerMode(Banner.Mode.OFF)
-                .run(args);
+//        new SpringApplicationBuilder()
+//                .sources(Application.class)
+//                .bannerMode(Banner.Mode.OFF)
+//                .run(args);
 
         // Startup mode 3 --default
 //        SpringApplication.run(Application.class, args);
--- src/main/java/com/choxsu/config/RunAfterRunner.java
+++ src/main/java/com/choxsu/config/RunAfterRunner.java
@@ -1,6 +1,8 @@
 package com.choxsu.config;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,14 +11,22 @@ import org.springframework.stereotype.Component;
  * 这两个接口都以相同的方式工作，并提供一个单一的run方法，
  * 该方法在SpringApplication.run()完成之前调用。
  *
+ * 如果定义了多个CommandLineRunner或ApplicationRunner bean，则必须以特定的顺序调用它们
+ * 您还可以实现org.springframework.core.Ordered接口或使用org.springframework.core.annotation.Order注释。
  * @author choxsu
  * @date 2019/7/1
  */
 @Component
-public class RunAfterRunner implements CommandLineRunner {
+public class RunAfterRunner implements CommandLineRunner, Ordered {
+
 
     @Override
     public void run(String... args) throws Exception {
         // Do something...
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
     }
 }

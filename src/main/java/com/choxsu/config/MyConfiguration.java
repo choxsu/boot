6c876ee2f4988a14460097ebfbf66cf40bc266f8
choxsu <choxsu@163.com>
2019-07-01 23:11:24 +0800

    spring boot demo MyBean.class application.yml

--- src/main/java/com/choxsu/config/MyConfiguration.java
+++ src/main/java/com/choxsu/config/MyConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 /**
  * @author choxsu
  * @date 2019/7/1
@@ -12,5 +14,14 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class MyConfiguration {
 
+
+    /**
+     * Initialization
+     */
+    @PostConstruct
+    public void openConnection() {
+        // ...
+        System.out.println("Initialization");
+    }
 
 }

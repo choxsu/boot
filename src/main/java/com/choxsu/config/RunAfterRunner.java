7cffbba3527dd44e7485e58b3f5d7a7513d0d033
choxsu <choxsu@163.com>
2019-07-14 10:57:47 +0800

    delete invalid import

--- src/main/java/com/choxsu/config/RunAfterRunner.java
+++ src/main/java/com/choxsu/config/RunAfterRunner.java
@@ -2,7 +2,6 @@ package com.choxsu.config;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**

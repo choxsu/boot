a022a36dc14d17c7ce8e15b626faf092b67cd7c9
choxsu <choxsu@163.com>
2019-07-14 18:28:42 +0800

    test action

--- src/test/java/test/choxsu/TestApplication.java
+++ src/test/java/test/choxsu/TestApplication.java
@@ -1,7 +1,11 @@
 package test.choxsu;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.choxsu.Application;
+import com.choxsu.common.entity.Account;
 import com.choxsu.config.ApplicationArgumentsConfig;
+import com.choxsu.service.AccountService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +13,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * @author choxsu
@@ -21,10 +27,21 @@ public class TestApplication {
     @Autowired
     ApplicationArgumentsConfig argumentsConfig;
 
+    @Autowired
+    private AccountService accountService;
+
 
     @Test
     public void debugInfo() {
         System.out.println("argumentsConfig.containsDebugOption() = " + argumentsConfig.containsDebugOption());
+    }
+
+    @Test
+    public void getAccounts() {
+        IPage iPage = new Page().setSize(2).setCurrent(1);
+        IPage page = accountService.page(iPage);
+        System.out.println(page.getRecords().toString());
+        System.out.println("page.getPages() = " + page.getPages());
     }
 
     @Before

33614b04289fccf4c6426fc572644bc4cf0db266
choxsu <choxsu@163.com>
2019-07-20 13:05:18 +0800

    a little update

--- src/main/java/com/choxsu/config/shiro/UserRealm.java
+++ src/main/java/com/choxsu/config/shiro/UserRealm.java
@@ -1,9 +1,11 @@
 package com.choxsu.config.shiro;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import com.choxsu.common.entity.Account;
+import com.choxsu.service.AccountService;
+import com.jfinal.kit.HashKit;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.realm.CachingRealm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,14 +15,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserRealm extends CachingRealm {
 
+    @Autowired
+    AccountService accountService;
 
     @Override
     public boolean supports(AuthenticationToken token) {
-        return false;
+        return token instanceof UsernamePasswordToken;
     }
 
     @Override
     public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-        return null;
+        String username = (String) token.getPrincipal();
+        String password = new String((char[]) token.getCredentials());
+        Account accountByUsername = accountService.findAccountByUsername(username);
+        if (accountByUsername == null) {
+            throw new UnknownAccountException();
+        }
+        String salt = accountByUsername.getSalt();
+        String hashedPass = HashKit.sha256(salt + password);
+        // 未通过密码验证
+        if (!accountByUsername.getPassword().equals(hashedPass)) {
+            throw new IncorrectCredentialsException();
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username, password, getName());
     }
 }

50c5870fad858053211b899ba3dd89da0ad5fa1a
choxsu <choxsu@163.com>
2019-08-25 13:05:59 +0800

    json返回结果 格式 约定

--- src/main/java/com/choxsu/common/ret/Ret.java
+++ src/main/java/com/choxsu/common/ret/Ret.java
@@ -14,28 +14,46 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Ret {
+    /**
+     * 参数错误码
+     */
+    private static final int PARAM_ERR = 10010;
+    /**
+     * 系统错误
+     */
+    private static final int SYS_ERR = 10011;
+
     private Integer status;
     private String msg;
     private Object data;
+
 
     public static Ret build() {
         return new Ret();
     }
 
     public static Ret ok(String msg, Object data) {
-        return new Ret(200, msg, data);
+        return new Ret(1, msg, data);
     }
 
     public static Ret ok(String msg) {
-        return new Ret(200, msg, null);
+        return new Ret(1, msg, null);
     }
 
     public static Ret error(String msg, Object data) {
-        return new Ret(500, msg, data);
+        return new Ret(0, msg, data);
     }
 
     public static Ret error(String msg) {
-        return new Ret(500, msg, null);
+        return new Ret(0, msg, null);
+    }
+
+    public static Ret paramError() {
+        return new Ret(PARAM_ERR, "参数错误，请检查参数！", null);
+    }
+
+    public static Ret sysError() {
+        return new Ret(SYS_ERR, "系统错误，请联系管理员！", null);
     }
 
 }

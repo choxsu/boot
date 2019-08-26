f03cdd538b3d9e2a9c40601780a2b79bd74eeb9a
choxsu <choxsu@163.com>
2019-08-26 16:18:05 +0800

    base util

--- src/main/java/com/choxsu/utils/BaseUtil.java
+++ src/main/java/com/choxsu/utils/BaseUtil.java
@@ -1,5 +1,8 @@
 package com.choxsu.utils;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
@@ -7,7 +10,11 @@ import org.springframework.stereotype.Component;
  * @date 2019/7/14
  */
 @Component
-public class BaseUtil {
+public class BaseUtil implements ApplicationContextAware {
 
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
 }
